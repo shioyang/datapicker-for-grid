c193646c825adcdb216da5787eb044c7c269c90e
shioyang <shioyang4321@gmail.com>
2018-02-13 09:17:07 +0900

    refactor: remove all semi-colon

--- content-script.js
+++ content-script.js
@@ -1,6 +1,6 @@
 
 $(function(){
-  console.log("START content script");
+  console.log("START content script")
 
   function addDatepicker() {
     console.log("[addDatepicker]")
@@ -14,41 +14,41 @@ $(function(){
               "<input id='datepicker-for-grid-input-" + index + "' type='text' disabled />" +
             "</div>" +
           "</div>"
-        );
+        )
 
         $(this).find("div.datepicker-for-grid.datepicker-area img")
           .on("click", function() {
-            $(this).parent().find("input").datepicker("show");
-          });
+            $(this).parent().find("input").datepicker("show")
+          })
 
         $("input#datepicker-for-grid-input-" + index).datepicker({
           dateFormat: "yy/mm/dd",
           onSelect: function(){
-            var dateObj = $(this).datepicker("getDate");
-            var value = $.datepicker.formatDate("yy/mm/dd", dateObj);
+            var dateObj = $(this).datepicker("getDate")
+            var value = $.datepicker.formatDate("yy/mm/dd", dateObj)
             // Copy it to clipboard
             chrome.runtime.sendMessage({
                 text: value.slice(2)
-            });
+            })
             // Set it to innerHTML of the input tab.
-            $(this).val(value.slice(2));
+            $(this).val(value.slice(2))
           }
-        });
+        })
 
-      });
+      })
 
   }
 
 
   $("body")
-    .append("<button id='datepicker-for-grid-button01' class='datepicker-for-grid trigger-button'>Date Picker</button>");
+    .append("<button id='datepicker-for-grid-button01' class='datepicker-for-grid trigger-button'>Date Picker</button>")
 
   $("#datepicker-for-grid-button01").on("click", function(){
-    addDatepicker();
-  });
+    addDatepicker()
+  })
 
-  console.log("END content script");
-});
+  console.log("END content script")
+})
 
 // http://dotnsf.blog.jp/archives/1058669933.html
 // https://qiita.com/ororog/items/146b7cdac85a48690c1e
