0446773af25c9e88cd7c493bd8d2005a4567ad61
shioyang <shioyang4321@gmail.com>
2018-02-13 09:25:47 +0900

    refactor: polish

--- content-script.js
+++ content-script.js
@@ -1,12 +1,11 @@
 
-$(function(){
-  console.log("START content script")
+$(function() {
 
   function addDatepicker() {
-    console.log("[addDatepicker]")
 
     $("div.list-grid div.x-toolbar div.x-box-target")
-      .each(function(index){
+      .each(function(index) {
+        // Insert DOMs, img and input.
         $(this).append(
           "<div class='x-box-item datepicker-for-grid datepicker-area'>" +
             "<img src='" + chrome.extension.getURL('resource/icon_x128.png') + "' alt='datepicker' width='16px' height='16px' />" +
@@ -16,25 +15,26 @@ $(function(){
           "</div>"
         )
 
+        // Handle click event on img, and show datapicker.
         $(this).find("div.datepicker-for-grid.datepicker-area img")
           .on("click", function() {
             $(this).parent().find("input").datepicker("show")
           })
 
+        // Attach datepicker UI to input.
         $("input#datepicker-for-grid-input-" + index).datepicker({
           dateFormat: "yy/mm/dd",
-          onSelect: function(){
+          onSelect: function() {
             var dateObj = $(this).datepicker("getDate")
             var value = $.datepicker.formatDate("yy/mm/dd", dateObj)
+
             // Copy it to clipboard
-            chrome.runtime.sendMessage({
-                text: value.slice(2)
-            })
+            chrome.runtime.sendMessage({ text: value.slice(2) })
+
             // Set it to innerHTML of the input tab.
             $(this).val(value.slice(2))
           }
         })
-
       })
 
   }
@@ -43,11 +43,11 @@ $(function(){
   $("body")
     .append("<button id='datepicker-for-grid-button01' class='datepicker-for-grid trigger-button'>Date Picker</button>")
 
-  $("#datepicker-for-grid-button01").on("click", function(){
-    addDatepicker()
-  })
+  $("#datepicker-for-grid-button01")
+    .on("click", function() {
+      addDatepicker()
+    })
 
-  console.log("END content script")
 })
 
 // http://dotnsf.blog.jp/archives/1058669933.html
